c4ebabadce5d3aa786d4bf64f9e94c0d77de6380
nkyadav4598 <nirajkumar4598@gmail.com>
2018-04-16 02:09:44 +0530

    14

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ $result = mysqli_query($connectdb, $query);
 <html>
 <head>
   <script type="text/javascript" src="amajon.js"></script>
-<title>Amajon</title>
+<title>eRation
+</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width , initial-scale= 1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
